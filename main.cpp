d819e4994827652cf41f5dc7565e13f9585ad092
esp_maniac <91507465+espmaniac@users.noreply.github.com>
2022-07-09 22:18:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main() {
 	image.bitCount = 24;
 	image.initBmp();
 
-	for (uint32_t i= 0; i < image.getAbsHeight(); ++i) {
+	for (int32_t i= 0; i < image.getAbsHeight(); ++i) {
 		for (uint32_t j = 0; j < image.getBytesWidth(); ++j)
-			image.getData()[abs(image.getFlip() - i)][j] = i * j * 155;
+			image.getData()[(int32_t)abs(image.getFlip() - i)][j] = i * j * 155;
 	}
 	
 	image.write("images/24bpp.bmp");
@@ -22,9 +22,9 @@ int main() {
 	image.initBmp();
 	image.getColors()[0] = {0,0,0,0};
 	image.getColors()[1] = {255,255,255,0};
-	for (uint32_t i= 0; i < image.getAbsHeight(); ++i) {
+	for (int32_t i= 0; i < image.getAbsHeight(); ++i) {
 		for (uint32_t j = 0; j < image.getBytesWidth(); ++j)
-			image.getData()[abs(image.getFlip() - i)][j] = 0b11110000;
+			image.getData()[(int32_t)abs(image.getFlip() - i)][j] = 0b11110000;
 	}
 	image.write("images/1bpp.bmp");
 	return 0;
